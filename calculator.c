1c44a5c5214c6238de03f88ce655174fd4f831eb
C. Sean Young <techsy730@gmail.com>
2021-02-18 10:35:26 -0600

    Fix leftover code from a bad interactive add.

--- calculator.c
+++ calculator.c
@@ -2253,10 +2253,6 @@ static void logIterationsAfterPB(int ID, int stepIndex, struct BranchPath * curN
  * is passed back to start.c to try submitting to the server.
  -------------------------------------------------------------------*/
 struct Result calculateOrder(int ID, long max_branches) {
-	if (!cook_container_pool_init) {
-		cook_container_pool = init_cook_container_pool_state();
-		cook_container_pool_init = true;
-	}
 	int randomise = getConfigInt("randomise");
 	int select = getConfigInt("select");
 	int debug = getConfigInt("debug");
