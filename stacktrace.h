8e4a58910c2d9eaaf79c69b4cf257a329c996f30
C. Sean Young <techsy730@gmail.com>
2021-07-03 16:20:17 -0500

    Fix various weird issues from mismatching `#if` `#endif` pairs

--- stacktrace.h
+++ stacktrace.h
@@ -3,22 +3,28 @@
 
 #include <stdio.h>
 #include "absl/base/port.h"
-#include "internal/base_essentials.h"
+
+// Holy crap, getting stack traces is a giant pain.
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#ifdef INCLUDE_STACK_TRACES
+#include "internal/base_essentials.h"
+#include "internal/base_assert.h"
+
+#if INCLUDE_STACK_TRACES
+#ifndef STACK_TRACE_FRAMES
 #define STACK_TRACE_FRAMES 40
 #define WIN_MAX_STACK_TRACE_FRAMES 63
+#endif
 _CIPES_STATIC_ASSERT(STACK_TRACE_FRAMES < WIN_MAX_STACK_TRACE_FRAMES, "Windows can only fetch up to " _XSTR(WIN_MAX_STACK_TRACE_FRAMES) " frames at one, so keep this below that.");
 ABSL_ATTRIBUTE_NOINLINE void printStackTraceF(FILE* f);
 #else
 ABSL_ATTRIBUTE_ALWAYS_INLINE inline void printStackTraceF(FILE* f) {}
 #endif
 
-#if defined(INCLUDE_STACK_TRACES) && _CIPES_IS_WINDOWS
+#if INCLUDE_STACK_TRACES && _CIPES_IS_WINDOWS
 // Prepare to initialize anything that needs to be done to initialize stacktracing.
 // Ideally this should only be called once per program, but will have logic to prevent multiple initializing from causing problems.
 // Also, if you fail to call this method, then printStackTraceF will call it for you, but is much more likely to fail at that point
